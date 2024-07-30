3d07be5e948622eb02630ebaa000a019dbeeedb9
omarlmv <omar.lemv@gmail.com>
2024-07-30 00:28:35 -0500

    git commit -m "add config security pom"

--- src/main/java/com/example/productapi/config/SecurityConfig.java
+++ src/main/java/com/example/productapi/config/SecurityConfig.java
@@ -2,15 +2,15 @@ package com.example.productapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,10 +19,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
-                authorize.requestMatchers("/public/**")
-                .permitAll().anyRequest().authenticated())
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges ->
+                exchanges.pathMatchers("/public/**")
+                        .permitAll().anyExchange()
+                        .authenticated())
                 .httpBasic(withDefaults());
         return http.build();
     }
