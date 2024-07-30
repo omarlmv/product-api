e9f6d840285bdb445f1806fbb14cf4f578c2b35e
omarlmv <omar.lemv@gmail.com>
2024-07-30 03:58:54 -0500

    git commit -m "log SecurityConfig"

--- src/main/java/com/example/productapi/config/CustomLoggingFilter.java
+++ src/main/java/com/example/productapi/config/CustomLoggingFilter.java
@@ -8,16 +8,16 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
-@Component
-public class CustomLoggingFilter extends UsernamePasswordAuthenticationFilter {
+//@Component
+public class CustomLoggingFilter { //extends UsernamePasswordAuthenticationFilter
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    //@Override
+    /*public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         logger.debug("Username: " + username);
         logger.debug("Password: " + password); // For demonstration purposes only
         UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
         return this.getAuthenticationManager().authenticate(authRequest);
-    }
+    }*/
 }
