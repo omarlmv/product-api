544e0da996d41310743f253d055e81bcbb9b97ec
omarlmv <omar.lemv@gmail.com>
2024-07-30 11:32:22 -0500

    git commit -m "config test"

---
+++ src/main/java/com/example/productapi/config/AuthenticationRequest.java
@@ -0,0 +1,9 @@
+package com.example.productapi.config;
+
+import lombok.Getter;
+
+@Getter
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
--- src/main/java/com/example/productapi/config/SecurityConfig.java
+++ src/main/java/com/example/productapi/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.productapi.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,66 +18,47 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        logger.debug("Configuring security filter chain");
-        http.authorizeHttpRequests(authorize -> {
-            logger.debug("Configuring authorization for /public/**");
-            authorize.requestMatchers("/public/**").permitAll()
-                    .anyRequest().authenticated();
-        }).httpBasic(withDefaults());
-        logger.debug("Returning configured SecurityFilterChain");
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    /*@Autowired
-    private CustomLoggingFilter customLoggingFilter;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        logger.debug("Configuring security filter chain");
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                //.addFilterBefore(customLoggingFilter, UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(withDefaults());
-        return http.build();
-    }*/
-
     @Bean
     public UserDetailsService userDetailsService() {
-        String rawPassword = "password";
-        String encodedPassword = passwordEncoder().encode(rawPassword);
-
-        System.out.println("Configuring in-memory user details service");
-        logger.debug("Configuring in-memory user details service");
-        UserDetails user = User.builder()
-                .username("user")
-                .password(encodedPassword)
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
-        System.out.println("In-memory user: " + user);
-        logger.debug("In-memory user: {}", user);
-        System.out.println("Raw password: " + rawPassword);
-        logger.debug("Raw password: " + rawPassword);
-        System.out.println("Raw encodedPassword: " + encodedPassword);
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        System.out.println("Configuring password encoder");
-        logger.debug("Configuring password encoder");
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 }
